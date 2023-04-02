dd13df3692f5b892daddcf1ead74ff521e0559aa
Nhat Dinh <nhatphutho@gmail.com>
2023-04-02 17:44:30 +0900

    Create ExtraStorage.sol

---
+++ ExtraStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import "./SimpleStorage.sol";
+
+contract ExtraStorage is SimpleStorage {
+    // virtual, override
+
+    function store(uint256 _favoriteNumber) public override {
+        favoriteNumber = _favoriteNumber + 5;
+    }
+}
